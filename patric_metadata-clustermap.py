6cdbc370bb36028e8d48de469f43cca01419b2b6
theoportlock <zn.tportlock@gmail.com>
2021-07-19 10:21:07 +0200

    New idea

--- patric_metadata-clustermap.py
+++ patric_metadata-clustermap.py
@@ -15,14 +15,28 @@ from statsmodels.stats.multitest import multipletests
 import dask.dataframe as dd
 
 samples_metadata = pd.read_csv('metadata.csv', index_col=0).dropna()._get_numeric_data()
-#samples_patric = pd.read_csv("patricNorm.csv", index_col=0).T
-samples_patric = dd.read_csv("patricNorm.csv")
+samples_patric = pd.read_csv("patricNorm.csv", index_col=0).T
 
 # Join patric information
 samples_patricMetadata = samples_patric.join(samples_metadata, how='inner')
 
+def get_correlations(df):
+    df = df.dropna()._get_numeric_data()
+    dfcols = pd.DataFrame(columns=df.columns)
+    pvalues = dfcols.transpose().join(dfcols, how="outer")
+    correlations = dfcols.transpose().join(dfcols, how="outer")
+    for ix, r in enumerate(df.columns):
+        for jx, c in enumerate(df.columns):
+            sp = spearmanr(df[r], df[c])
+            correlations[c][r] = sp[0]
+            pvalues[c][r] = sp[1] if ix > jx else np.nan  # Only store values below the diagonal
+    return correlations.astype("float"), pvalues.astype("float")
+
+
 # Calculate and format correlations
-correlationArray, uncorrectedPValueArray = spearmanr(samples_patricMetadata)
+#correlationArray, uncorrectedPValueArray = spearmanr(samples_patricMetadata)
+correlationArray, uncorrectedPValueArray = get_correlations(samples_patricMetadata)
+
 correlations = pd.DataFrame(
     correlationArray,
     index=samples_patricMetadata.columns,
@@ -31,18 +45,21 @@ uncorrectedPValues = pd.DataFrame(
     uncorrectedPValueArray,
     index=samples_patricMetadata.columns,
     columns=samples_patricMetadata.columns)
+
 slicedCorrelations = correlations.loc[
     samples_patric.columns,
     samples_metadata.columns]
 slicedUncorrectedPValues = uncorrectedPValues.loc[
     samples_patric.columns,
     samples_metadata.columns]
+
 filteredSlicedUncorrectedPValues = slicedUncorrectedPValues.loc[
     (slicedCorrelations.sum(axis=1) != 0),
     (slicedCorrelations.sum(axis=0) != 0)]
 filteredSlicedCorrelations = slicedCorrelations.loc[
     (slicedCorrelations.sum(axis=1) != 0),
     (slicedCorrelations.sum(axis=0) != 0)]
+
 significantMatrix = pd.DataFrame(
     multipletests(filteredSlicedUncorrectedPValues.values.flatten())[0].reshape(filteredSlicedUncorrectedPValues.shape),
     index = filteredSlicedUncorrectedPValues.index,
